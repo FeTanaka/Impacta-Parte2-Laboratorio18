81bcfd507a084730266b9c1af34e01407b1c90d2
curso <curso@curso.com>
2022-07-27 20:38:39 -0300

    implementação do código do fragmento

--- app/src/main/java/br/com/impacta/parte2_laboratorio18/HomeFragment.kt
+++ app/src/main/java/br/com/impacta/parte2_laboratorio18/HomeFragment.kt
@@ -5,55 +5,84 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import br.com.impacta.parte2_laboratorio18.data.Contato
+import br.com.impacta.parte2_laboratorio18.databinding.FragmentHomeBinding
+import org.json.JSONArray
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private var jsonArray: JSONArray? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listaContato = gerarContatos(5)
+
+        binding.button.setOnClickListener {
+            jsonArray = gerarJSONArray(listaContato)
+            binding.textView2.text = jsonArray.toString()
+        }
+
+        binding.button2.setOnClickListener {
+            val lista = recuperarListaContato()
+            var stringExibicao = ""
+            for (i in lista.indices) {
+                stringExibicao = "${stringExibicao}\n${lista[i].toString()}"
             }
+            binding.textView2.text = stringExibicao
+        }
+    }
+
+    fun gerarContatos(quantidade: Int): MutableList<Contato> {
+        val lista = mutableListOf<Contato>()
+        for (i in 1..quantidade) {
+            var contatoGerado = Contato(
+                idContato = i.toLong(),
+                nome = "Contato ${i}",
+                idade = i * 2
+            )
+            lista.add(contatoGerado)
+        }
+        return lista
+    }
+
+    fun gerarJSONArray(lista: MutableList<Contato>): JSONArray {
+        val jsonArray = JSONArray()
+        lista.forEach {
+            jsonArray.put(it.gerarJSON())
+        }
+        return jsonArray
+    }
+
+    fun recuperarContato(json: JSONObject): Contato {
+        val idContato = json.get("idContato").toString().toLong()
+        val nome = json.get("nome").toString()
+        val idade = json.get("idade").toString().toInt()
+        return Contato(idContato, nome , idade)
+    }
+
+    fun recuperarListaContato(): MutableList<Contato> {
+        val lista = mutableListOf<Contato>()
+
+        if (jsonArray != null) {
+            for (i in 0 until jsonArray!!.length()) {
+                lista.add(recuperarContato(jsonArray!![i] as JSONObject))
+            }
+        }
+
+        return lista
     }
 }
