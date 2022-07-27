3d498bb4da42594c96efc6a6f39b05cf21390efd
curso <curso@curso.com>
2022-07-27 19:30:15 -0300

    commit inicial

---
+++ app/src/main/java/br/com/impacta/parte2_laboratorio18/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.impacta.parte2_laboratorio18
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
