de1d1e23dd4a7bb615c8a5cc74d2ea62dfe73389
curso <curso@curso.com>
2022-07-27 20:37:07 -0300

    método que gera um objeto json de um contato

--- app/src/main/java/br/com/impacta/parte2_laboratorio18/data/Contato.kt
+++ app/src/main/java/br/com/impacta/parte2_laboratorio18/data/Contato.kt
@@ -1,7 +1,17 @@
 package br.com.impacta.parte2_laboratorio18.data
 
+import org.json.JSONObject
+
 data class Contato(
     val idContato: Long,
     val nome: String,
     val idade: Int
-)
+) {
+    fun gerarJSON(): JSONObject {
+        val json = JSONObject()
+        json.put("idContato", this.idContato)
+        json.put("nome", this.nome)
+        json.put("idade", this.idade)
+        return json
+    }
+}
